4c0e47f7b20d6e82409a6c8cfeb1b58181ab4232
mkalme <mikelis.kalme@gmail.com>
2025-05-22 18:00:19 +0300

    Add project files.

---
+++ CustomScrollBar/ScrollPanel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomScrollBar {
+    public class ScrollPanel : Panel{
+        private Panel ContentPanel { get; set; }
+        public new ControlCollection Controls {
+            get { return ContentPanel.Controls; }
+        }
+
+        public ScrollPanel() {
+            ContentPanel = new Panel();
+            ContentPanel.AutoSize = true;
+            ContentPanel.BackColor = System.Drawing.Color.FromArgb(60, 60, 60);
+
+            Button button = new Button();
+            button.AutoSize = true;
+            Controls.Add(button);
+
+            ContentPanel.Padding = new Padding(0);
+            ContentPanel.Margin = new Padding(0);
+
+            base.Controls.Add(ContentPanel);
+        }
+    }
+}
---
+++ GUI/Window.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using CustomScrollBar;
+
+namespace GUI {
+    public partial class Window : Form {
+        public Window()
+        {
+            InitializeComponent();
+
+            ScrollPanel panel = new ScrollPanel();
+            panel.Dock = DockStyle.Fill;
+            panel.BackColor = Color.FromArgb(50, 50, 50);
+
+            Controls.Add(panel);
+        }
+    }
+}
